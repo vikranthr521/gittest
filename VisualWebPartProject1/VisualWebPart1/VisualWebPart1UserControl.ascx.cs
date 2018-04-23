33881fffc1c869f8f048f81243339c073da90e93
Reddy <vikranthr@supergas.in>
2018-04-23 17:56:04 +0530

    Add project files.

---
+++ VisualWebPartProject1/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace VisualWebPartProject1.VisualWebPart1
+{
+    public partial class VisualWebPart1UserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SPSite site = new SPSite("http://spdevapp"))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    SPList list = web.Lists.TryGetList("Employee");
+                    SPListItemCollection collection = list.Items;
+                    gvList.DataSource = collection.GetDataTable();
+                    gvList.DataBind();
+                }
+            }
+        }
+    }
+}
