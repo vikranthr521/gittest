91edfe85c6820d49ecd1c7c6c8b7c311e512425f
vikranthr <vikranthr@SPDEVAPP>
2018-04-23 17:59:00 +0530

    this is for testing

--- VisualWebPartProject1/VisualWebPart1/VisualWebPart1.cs
+++ VisualWebPartProject1/VisualWebPart1/VisualWebPart1.cs
@@ -19,6 +19,7 @@ namespace VisualWebPartProject1.VisualWebPart1
         {
             Control control = Page.LoadControl(_ascxPath);
             Controls.Add(control);
+            //
         }
     }
 }
